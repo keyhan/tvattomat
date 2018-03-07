106cefe71190d006905d97a5559022ffeac749f1
Keyhan Hadjari <keyhan.hadjari@gmail.com>
2018-03-07 08:17:15 +0100

    More advanced features

--- src/main/java/se/pensionsmyndigheten/se/tvattomat/TvattomatApplication.java
+++ src/main/java/se/pensionsmyndigheten/se/tvattomat/TvattomatApplication.java
@@ -8,6 +8,9 @@ import se.pensionsmyndigheten.se.tvattomat.domain.Excerpt;
 import se.pensionsmyndigheten.se.tvattomat.repository.BookRepository;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class TvattomatApplication {
@@ -25,15 +28,27 @@ public class TvattomatApplication {
 	@PostConstruct
 	private void initData() {
 
+		List<Book> books = new ArrayList<>();
+		for (int i = 0; i <1000; i++) {
+			books.add(Book.builder()
+					.author("Keyhan Hadjari" + i)
+					.title("Book of Aliens" + i)
+					.genre("SciFi" + i)
+					.isbn("1111111" + i)
+					.published(1950 + i)
+					.excerpt(new Excerpt(1, "Be aware of the aliens!!!"))
+					.build());
+		}
+		bookRepository.save(books);
 
-		bookRepository.save(Book.builder()
-				.author("Keyhan Hadjari")
-				.title("Book of Aliens")
-				.genre("SciFi")
-				.isbn("1111111")
-				.published(1998)
-				.excerpt(new Excerpt(1, "Be aware of the aliens!!!"))
-				.build()
-		);
+//		bookRepository.save(Book.builder()
+//				.author("Keyhan Hadjari")
+//				.title("Book of Aliens")
+//				.genre("SciFi")
+//				.isbn("1111111")
+//				.published(1998)
+//				.excerpt(new Excerpt(1, "Be aware of the aliens!!!"))
+//				.build()
+//		);
 	}
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -13,3 +13,7 @@ CREATE TABLE book (
 );
 
 CREATE SEQUENCE seq_book MINVALUE 1 START WITH 1 INCREMENT BY 1 CACHE 10;
+
+-- UPDATE BOOK SET BOOK.EXCERPT = UPDATEXML(BOOK.EXCERPT, '/excerpt/@chapter', '3', '/excerpt/text/text()', 'Changed Text')  WHERE ID=1;
+
+-- SELECT UPDATEXML(BOOK.EXCERPT, '/excerpt/@chapter', '3', ) as "ex1" FROM BOOK WHERE ID=1
